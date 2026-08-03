8eee4fe315c8dcf1285d1132451da5288d1c4bb1
Nolin <razuanulnolin@gmail.com>
2026-08-03 16:05:28 +0600

    This is the first commit for my AI journey

---
+++ operations.py
@@ -0,0 +1,5 @@
+age = 25
+is_eligable = True
+
+can_vote = age >= 18 and is_eligable
+print(can_vote)
---
+++ variable.py
@@ -0,0 +1,21 @@
+name = "Nolin"
+
+age = 22
+
+is_student = True
+
+string = "My name is nolin"
+lone_string = """
+my name is nolin.
+I am a student
+"""
+
+first_name = "Razuanul"
+last_name = "Hoque"
+
+print("-" * 10)
+
+full_name = first_name + " " + last_name
+print(full_name)
+
+print("-" * 10)
