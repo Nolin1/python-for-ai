ae7a04ab164488c707c491dd872c28c2c24b3a14
Nolin <razuanulnolin@gmail.com>
2026-08-14 20:22:47 +0600

    did some formatting using Ruff

--- testing_environment.py
+++ testing_environment.py
@@ -1,12 +1,13 @@
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
 
 # Load the .env file
 load_dotenv()
 
 # Now use your variables
-api_key = os.environ.get('API_KEY')
-debug = os.environ.get('DEBUG')
+api_key = os.environ.get("API_KEY")
+debug = os.environ.get("DEBUG")
 
 print(f"API Key: {api_key}")
 print(f"Debug mode: {debug}")
